61a4c896d681491f707a7c3a3ef09356cd9a60bd
Mohammed Sarfaraz <sarfarazuddin9999@gmail.com>
2026-03-12 20:19:51 +0530

    fix: update contact email in site configuration

--- src/Config/Site.ts
+++ src/Config/Site.ts
@@ -6,7 +6,7 @@ export const siteConfig = {
     "Work experience & Professional Contracting Services And started our business by doing decorative painting and stenciled wood floors. The company then expanded into decorative stamped concrete floors. Topcoat artisans experimented with polymered concrete and modified concrete to apply to vertical surfaces and ceilings; they used a variety of techniques including spraying, troweling, and brushing , we are also professionals in all types of floors such as epoxy and terrazzo works.",
   url: "https://topcoat-llc.net",
   phone: "201 315 2633",
-  email: "sales@topcoat-llc.net",
+  email: "Almahairi@gmail.com",
   address: "312 Fair Lawn Ave, Fair Lawn, NJ 07410",
   services: [
     { name: "Epoxy Flooring", slug: "epoxy-flooring" },
